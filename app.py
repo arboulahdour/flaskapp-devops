c0d8f67520a0c00352aaa0cb259a79a25f1101e6
admin <admin@DESKTOP-89TQ7CH.comparex.dz>
2021-08-31 22:41:44 +0100

    edit app

--- app.py
+++ app.py
@@ -4,6 +4,6 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-  return '<body style="background: black; color: white;"><h1><center>Flask Application For DevOps Lab</center></h1></body>'
+  return '<body style="background: #0b131f; color: white; font-size: 30px; margin: 100px; letter-spacing: 2.5px;"><h1><center>--- Flask Application For DevOps Lab ---</center></h1></body>'
 
 app.run(host='0.0.0.0', port=5000)
